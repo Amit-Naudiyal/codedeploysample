6b9f92e0a3a4a58ccb8a872b383716310062f7c5
Naudiyal <anaudiya@9801a7ab9fad.ant.amazon.com>
2019-08-27 22:12:45 +1000

    add afterinstall

---
+++ AfterInstall.ps1
@@ -0,0 +1,6 @@
+###Create some content
+
+New-Item -Path "C:\Viewpost\AWSDeploy\Packages" -Name "testfile1.txt" -ItemType "file" -Value "This is a text string."
+
+
+New-Item -Path "C:\Viewpost\AWSDeploy\Packages" -Name "logfiles" -ItemType "directory"
